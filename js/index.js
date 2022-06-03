ee1a3b17b4647e441bdc9c2eab2b1eefbbc8ec5c
mauricio.iniestaz@elektra.com.mx <mauricio.iniestaz@elektra.com.mx>
2022-06-03 02:51:33 -0500

    Actualizacion de la data y se agregaron mas imagenes

--- js/index.js
+++ js/index.js
@@ -2,9 +2,6 @@ const verPromociones = () => {
   let container = document.querySelector(".content");
 
   promociones.forEach((element) => {
-    let date = new Date(element.vigencia);
-    let fecha =
-      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
     container.innerHTML +=
         `<a href=\"detalle.html?id=${element.id}\">` +
         '<div class="journal__entry">' +
@@ -20,10 +17,12 @@ const verPromociones = () => {
                         `${element.calificacion}` + 
                     "</p>" +
                     `<label><img src=\"img/score_on.png\" class="star" /></label>`+
+                    `<span class="separacion"> | </span>`+
                     `<label><img src=\"img/discount_off.png\" class="disxount_off" /></label>`+
                     `<label class="discount_text">Gana hasta $150</label>`+
-               `</div>`+
-                
+                `</div>`+
+                    
+                `<span class="giro">${element.giro}</span>`+
                 `<p class="journal__entry-content"> ${element.direccion} </p>` +
             "</div>" +
         "</div>" +
